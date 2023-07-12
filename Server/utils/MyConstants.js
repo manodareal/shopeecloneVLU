5a2ee9b8c2353285e5e44b51a6895e30aab08871
manodareal <106855122+manodareal@users.noreply.github.com>
2023-07-12 23:37:03 +0700

    wa da

---
+++ Server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'shoppingonline.bkgr2t6.mongodb.net',
+    DB_USER: 'tranquoc1981',
+    DB_PASS: 'Clubpenguin2014',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'tranquoc1981@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'lvdihvzjkbbpfkrw',
+    JWT_SECRET: '12345',
+    JWT_EXPIRES: '3600000', // in milliseconds
+  };
+  module.exports = MyConstants;
